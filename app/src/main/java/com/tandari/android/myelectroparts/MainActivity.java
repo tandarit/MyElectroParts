1f6c4bd97d25531263330b690dda17238da6fc48
Tamas Tandari <tandari.tamas@gmail.com>
2019-05-05 12:44:29 +0200

    Create a new branch with Botton Navigation Bar

--- app/src/main/java/com/tandari/android/myelectroparts/MainActivity.java
+++ app/src/main/java/com/tandari/android/myelectroparts/MainActivity.java
@@ -1,14 +1,19 @@
 package com.tandari.android.myelectroparts;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.tandari.android.myelectroparts.Models.Project;
 
@@ -16,8 +21,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private SQLiteDatabaseAdapter mSQLiteDatabaseAdapter;
+    private ActionBar mActionBar;
+
+
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -33,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
         mSQLiteDatabaseAdapter=new SQLiteDatabaseAdapter(getApplicationContext());
 
+        mActionBar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.botton_navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        //mActionBar.setTitle(R.string.app_name);
+
+
+        /*
         toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
@@ -44,8 +61,36 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.main_tabs);
         tabLayout.setupWithViewPager(viewPager);
         //setupTabIcons();
+        */
     }
 
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment;
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_projects:
+                //mActionBar.setTitle("Projects");
+                return true;
+            case R.id.navigation_part_list:
+                //mActionBar.setTitle("My Part List");
+                return true;
+            case R.id.navigation_wish_list:
+                //mActionBar.setTitle("My Wish List");
+                return true;
+
+        }
+        return false;
+    }
+
+    /*
     private void setupViewPager(ViewPager viewPager) {
 
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -85,4 +130,5 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentTitleList.get(position);
         }
     }
+    */
 }
